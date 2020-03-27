8323d5d22af4c55f30b9627d166af17d8528a550
David Ferrante <dmf07@hotmail.com>
2020-03-26 21:19:54 -0400

    better login layout

--- src/app/google-svg/google-svg.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-google-svg',
-  templateUrl: './google-svg.component.html',
-  styleUrls: ['./google-svg.component.scss']
-})
-export class GoogleSvgComponent {}
